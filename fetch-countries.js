c33699f06f25b197cd1d1dff6ad7abc47b0cc94b
ronin314 <56972039+ronin314@users.noreply.github.com>
2021-01-27 12:49:13 +0200

    Add files via upload

---
+++ fetch-countries.js
@@ -0,0 +1,10 @@
+import advices from './advices';
+
+function fetchCountries(searchQuery) {
+  const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
+  return fetch(url)
+    .then(response => response.json())
+    .catch(advices.onError);
+}
+
+export default fetchCountries;
